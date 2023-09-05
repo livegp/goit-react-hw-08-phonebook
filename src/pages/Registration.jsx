37b4ddcbabbabcf15625750b7472812ce2e0bb90
Oleksandr <live.gp@gmail.com>
2023-09-05 21:56:24 +0300

    Update Registration.jsx

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,4 +1,4 @@
-import RegistrationForm from '../components/RegistrationForm/RegistrationForm';
+import RegistrationForm from '../components/RegisterForm/RegisterForm';
 
 function Login() {
   return <RegistrationForm />;
