4804066062a549f799eec26c9d0b65193c80d342
Oleksandr <live.gp@gmail.com>
2023-09-05 21:56:06 +0300

    fix

---
+++ src/redux/auth.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://connections-api.herokuapp.com/',
+  }),
+  endpoints: builder => ({
+    registerUser: builder.mutation({
+      query: formData => ({
+        url: 'users/signup',
+        method: 'POST',
+        body: formData,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: formData => ({
+        url: 'users/login',
+        method: 'POST',
+        body: formData,
+      }),
+    }),
+    logOutUser: builder.mutation({
+      query: () => 'users/logout',
+    }),
+    refreshUser: builder.query({
+      query: () => 'users/current',
+    }),
+  }),
+});
+
+export const {
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useLogOutUserMutation,
+  useRefreshUserQuery,
+} = api;
+
+export default api;
